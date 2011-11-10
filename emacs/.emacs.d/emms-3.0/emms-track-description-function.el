9a39933995b228806264158da3d54e97e043adfc
xxxcjr <xxxcjr@sohu.com>
2011-11-10 20:27:51 +0800

    删除目录要用git rm -r命令啊啊啊！！！

--- emacs/.emacs.d/emms-3.0/emms-track-description-function.el
+++
@@ -1,8 +0,0 @@
-(defun kid-emms-info-track-description (track)
-  "Return a description of the current track."
-  (let ((artist (emms-track-get track 'info-artist))
-        (title (emms-track-get track 'info-title)))
-    (format "%-10s +| %s"
-            (or artist
-                "")
-            title)))
